71354dbcd97536c6d81b971adb2f7a33b8365991
Thomas Mock <j.thomasmock@gmail.com>
2021-09-03 13:30:12 -0500

    add gt_merge_stack for merging columns and stacking the text.

---
+++ R/merge_and_stack.R
@@ -0,0 +1,53 @@
+#' Merge and stack text from two columns in `gt`
+#'
+#' @description
+#' The `gt_merge_stack()` function takes an existing `gt` table and merges
+#' column 1 and column 2, stacking column 1's text on top of column 2's.
+#' Top text is in all caps with black bold text, while the lower text is smaller
+#' and dark grey.
+#'
+#' @param gt_object An existing gt table object of class `gt_tbl`
+#' @param col1 The column to stack on top. Will be converted to all caps, with black and bold text.
+#' @param col2 The column to merge and place below. Will be smaller and dark grey.
+#' @return An object of class `gt_tbl`.
+#' @importFrom gt %>%
+#' @export
+#' @import gt glue
+#' @examples
+#' team_df <- readRDS(url("https://github.com/nflverse/nflfastR-data/raw/master/teams_colors_logos.rds"))
+#'
+#' stacked_tab <- team_df %>%
+#'  dplyr::select(team_nick, team_abbr, team_conf, team_divison, team_wordmark) %>%
+#'  head(8) %>%
+#'  gt(groupname_col = "team_conf") %>%
+#'  gt_merge_stack(col1 = team_nick, col2 = team_divison) %>%
+#'  gt_img_rows(team_wordmark)
+#'
+#' @section Figures:
+#' \if{html}{\figure{merge-stack.png}{options: width=50\%}}
+#'
+#' @family Utilities
+#' @section Function ID:
+#' 1-4
+
+gt_merge_stack <- function(gt_object, col1, col2) {
+  col_bare <- rlang::enexpr(col2) %>% rlang::as_string()
+  # segment data with bare string column name
+  data_in <- gt_object[["_data"]][[col_bare]]
+
+  gt_object %>%
+    text_transform(
+      locations = cells_body(
+        columns = {{ col1 }}
+      ),
+      fn = function(x) {
+        glue::glue(
+        "<div style='line-height:10px'><span style='font-weight:bold;font-variant:small-caps;font-size:14px'>{x}</div>
+        <div style='line-height:12px'><span style ='font-weight:bold;color:grey;font-size:10px'>{data_in}</span></div>"
+        )
+      }
+    ) %>%
+    cols_hide(columns = {{ col2 }})
+}
+
+

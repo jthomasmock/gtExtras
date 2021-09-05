cf753df4dc7a4c4dc08dfeadb796a066d5090544
Thomas Mock <j.thomasmock@gmail.com>
2021-09-05 15:39:24 -0500

    add logic to gt_theme_guardian for top border color

--- R/gt_theme_guardian.R
+++ R/gt_theme_guardian.R
@@ -23,7 +23,7 @@ gt_theme_guardian <- function(gt_object,...) {
 
   stopifnot("Input must be a gt table" = "gt_tbl" %in% class(gt_object))
 
-  gt_object %>%
+  tab_out <- gt_object %>%
     opt_table_font(
       font = list(
         google_font("Noto Sans"),
@@ -32,18 +32,26 @@ gt_theme_guardian <- function(gt_object,...) {
     ) %>%
     tab_style(
       style = cell_borders(
-        sides = "top", color = "red", weight = px(0)
+        sides = "top", color = "white", weight = px(0)
       ),
       locations = cells_body(rows = 1)
     ) %>%
+    tab_style(
+      style = cell_text(color = "#005689", size = px(22), weight = 700),
+      locations = list(cells_title(groups = "title"),
+                       cells_title(groups = "subtitle")
+      )
+    )
+
+  tab_out <- tab_out %>%
     tab_options(
       row.striping.include_table_body = TRUE,
       table.background.color = "#f6f6f6",
       row.striping.background_color = "#ececec",
       column_labels.background.color = "#f6f6f6",
       column_labels.font.weight = "bold",
-      table.border.top.width = px(3),
-      table.border.top.color = "#ffffff00",
+      table.border.top.width = px(1),
+      table.border.top.color = "#40c5ff",
       table.border.bottom.width = px(3),
       table.border.bottom.color = "#ffffff00",
       footnotes.border.bottom.width = px(0),
@@ -58,7 +66,9 @@ gt_theme_guardian <- function(gt_object,...) {
       row_group.border.bottom.color = "grey",
       row_group.font.weight = "bold",
       column_labels.border.top.width = px(1),
-      column_labels.border.top.color = "#40c5ff",
+      column_labels.border.top.color = if(is.null(tab_out[["_heading"]][["title"]])){
+        "#40c5ff"
+      }else{"#ececec"},
       column_labels.border.bottom.width = px(2),
       column_labels.border.bottom.color = "#ececec",
       heading.border.bottom.width = px(0),

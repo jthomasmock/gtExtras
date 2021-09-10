d9526ec78e4118ff695fe85a33d09120d55ad7cb
Thomas Mock <j.thomasmock@gmail.com>
2021-09-10 11:49:27 -0500

    add gt_theme_dot_matrix - for @ajreinhard

---
+++ R/gt_theme_dot_matrix.R
@@ -0,0 +1,50 @@
+#' Apply dot matrix theme to a gt table
+#'
+#' @param gt_object An existing gt table object of class `gt_tbl`
+#' @param ... Additional arguments passed to `gt::tab_options()`
+#' @return An object of class `gt_tbl`.
+#' @importFrom gt %>%
+#' @export
+#' @import gt
+#' @examples
+#' library(gt)
+#' themed_tab <- head(mtcars) %>%
+#'   gt() %>%
+#'   gt_theme_dot_matrix() %>%
+#'   tab_header(title = "Styled like dot matrix printer paper")
+#' @section Figures:
+#' \if{html}{\figure{gt_dot_matrix.png}{options: width=100\%}}
+#'
+#' @family Themes
+#' @section Function ID:
+#' 1-5
+
+gt_theme_dot_matrix <- function(gt_object, ..., color = "#b5dbb6"){
+  gt_object %>%
+    opt_row_striping() %>%
+    tab_style(
+      style = cell_borders(sides = "bottom", weight = px(3), color = "#00000000"),
+      locations = list(
+        cells_body(rows = nrow(gt_object[["_data"]]))
+      )
+    ) %>%
+    opt_table_font(font = "Courier") %>%
+    tab_options(
+      ...,
+      heading.align = "left",
+      heading.border.bottom.color = "#00000000",
+      column_labels.text_transform = "lowercase",
+      column_labels.font.size = pct(85),
+      column_labels.border.top.style = "none",
+      column_labels.border.bottom.color = "black",
+      column_labels.border.bottom.width = px(2),
+      table.border.bottom.style = "none",
+      table.border.bottom.width = px(2),
+      table.border.bottom.color = "#00000000",
+      table.border.top.style = "none",
+      row.striping.background_color = color,
+      table_body.hlines.style = "none",
+      table_body.vlines.style = "none",
+      data_row.padding = px(1)
+    )
+}

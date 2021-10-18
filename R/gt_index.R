724c9c2094ed947662a82379387608852ecf998a
Thomas Mock <j.thomasmock@gmail.com>
2021-10-18 14:05:01 -0500

    add gt_index, update docs, add test for gt_index

---
+++ R/gt_index.R
@@ -0,0 +1,109 @@
+#' Return the underlying data, arranged by the internal index
+#' @description This is a utility function to extract the underlying data from
+#' a `gt` table. You can use it with a saved `gt` table, in the pipe (`%>%`)
+#' or even within most other `gt` functions (eg `tab_style()`). It defaults to
+#' returning the column indicated as a vector, so that you can work with the
+#' values. Typically this is used with logical statements to affect one column
+#' based on the values in that specified secondary column.
+#' Alternatively, you can extract the entire ordered data according to the
+#' internal index as a `tibble`. This allows for even more complex steps
+#' based on multiple indices.
+#'
+#' @param gt_object An existing gt table object
+#' @param column The column name that you intend to extract, accepts tidyeval semantics (ie `mpg` instead of `"mpg"`)
+#' @param as_vector A logical indicating whether you'd like just the column indicated as a vector, or the entire dataframe
+#'
+#' @return A vector or a `tibble`
+#' @export
+#'
+#' @examples
+#' library(gt)
+#'
+#' # This is a key step, as gt will create the row groups
+#' # based on first observation of the unique row items
+#' # this sampling will return a row-group order for cyl of 6,4,8
+#'
+#' set.seed(1234)
+#' sliced_data <- mtcars %>%
+#'   dplyr::group_by(cyl) %>%
+#'   dplyr::slice_head(n = 3) %>%
+#'   dplyr::ungroup() %>%
+#'   # randomize the order
+#'   dplyr::slice_sample(n = 9)
+#'
+#' # not in "order" yet
+#' sliced_data$cyl
+#'
+#' # But unique order of 6,4,8
+#' unique(sliced_data$cyl)
+#'
+#' # creating a standalone basic table
+#' test_tab <- sliced_data %>%
+#'   gt(groupname_col = "cyl")
+#'
+#' # can style a specific column based on the contents of another column
+#' tab_out_styled <- test_tab %>%
+#'   tab_style(locations = cells_body(mpg, rows = gt_index(., am) == 0),
+#'             style = cell_fill("red")
+#'   )
+#'
+#' # OR can extract the underlying data in the "correct order"
+#' # according to the internal gt structure, ie arranged by group
+#' # by cylinder, 6,4,8
+#' gt_index(test_tab, mpg, as_vector = FALSE)
+#'
+#' # note that the order of the index data is
+#' # not equivalent to the order of the input data
+#' # however all the of the rows still match
+#' sliced_data
+#' @section Figures:
+#' \if{html}{\figure{gt_index_style.png}{options: width=20\%}}
+#'
+#' @family Utilities
+#' @section Function ID:
+#' 2-20
+
+gt_index <- function(gt_object, column, as_vector = TRUE){
+
+  stopifnot("'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?" = "gt_tbl" %in% class(gt_object))
+  stopifnot("'as_vector' must be a TRUE or FALSE" = is.logical(as_vector))
+
+  if(isTRUE(identical(gt_object[["_row_groups"]],character(0)))){
+
+    # if the data is not grouped, then it will just "work"
+    df_ordered <- gt_object[["_data"]]
+
+  } else {
+
+    # if the data is grouped you need to identify the group column
+    # and arrange by that column. I convert to a factor so that the
+    # columns don't default to arrange by other defaults
+    #  (ie alphabetical or numerical)
+    gt_row_grps <- gt_object[["_row_groups"]]
+
+    gt_boxhead <- gt_object[["_boxhead"]]
+
+    # slice index to only the row_group variable
+    grp_col_name <- gt_boxhead[["var"]][gt_boxhead[["type"]]=="row_group"]
+
+    # get this as a tidyeval column
+    grp_col_sym <- rlang::sym(grp_col_name)
+
+    #  now it's "just" a df with tidyeval
+    df_ordered <- gt_object[["_data"]] %>%
+      # need to use walrus equal to get the !! semantics to work
+      dplyr::mutate(!!grp_col_sym := factor(!!grp_col_sym,
+                                            levels = gt_row_grps)) %>%
+      dplyr::arrange(!!grp_col_sym) %>%
+      dplyr::mutate(!!grp_col_sym := as.character(!!grp_col_sym))
+  }
+
+  # return as vector or tibble in correct, gt-indexed ordered
+  if(isTRUE(as_vector)){
+    df_ordered %>%
+      dplyr::pull({{column}})
+  } else {
+    df_ordered
+  }
+
+}
---
+++ tests/testthat/test-gt_index.R
@@ -0,0 +1,87 @@
+# Function to skip tests if Suggested packages not available on system
+check_suggests <- function() {
+  skip_if_not_installed("rvest")
+  skip_if_not_installed("xml2")
+}
+
+test_that("gt_index has correct inputs, correct ouput index, and can affect correct rows", {
+  check_suggests()
+
+  # This is a key step, as gt will create the row groups
+  # based on first observation of the unique row items
+  # this sampling will return a row-group order for cyl of 6,4,8
+
+  set.seed(1234)
+  sliced_data <- mtcars %>%
+    dplyr::group_by(cyl) %>%
+    dplyr::slice_head(n = 3) %>%
+    dplyr::ungroup() %>%
+    # randomize the order
+    dplyr::slice_sample(n = 9)
+
+  # not in "order" yet
+  raw_order <- sliced_data$cyl
+
+  # But unique order of 6,4,8
+  unique_order <- unique(sliced_data$cyl)
+
+# Expect input order to match ---------------------------------------------
+
+
+  expect_equal(c(6,4,8), unique_order)
+  expect_equal(c(6, 6, 6, 4, 8, 4, 8, 8, 4), raw_order)
+
+  # creating a standalone basic table
+  test_tab <- sliced_data %>%
+    gt::gt(groupname_col = "cyl")
+
+  # can style a specific column based on the contents of another column
+  tab_out_styled <- test_tab %>%
+    gt::tab_style(locations = cells_body(mpg, rows = gt_index(., am) == 0),
+              style = cell_fill("red")
+    ) %>%
+    gt::as_raw_html() %>%
+    rvest::read_html()
+
+  tab_values <- tab_out_styled %>%
+    rvest::html_elements("td:nth-child(1)") %>%
+    rvest::html_text()
+
+  tab_styled <- tab_out_styled %>%
+    rvest::html_elements("td:nth-child(1)") %>%
+    rvest::html_attr("style")
+
+  tab_pattern <- "background-color: \\s*(.*?)\\s*;"
+  reg_matches <- regmatches(tab_styled, regexec(tab_pattern, tab_styled)) %>%
+    lapply(function(x){x[2]}) %>%
+    unlist()
+
+
+# Expect color backgrounds to match ---------------------------------------
+
+
+  expect_equal(reg_matches, c("#FFFFFF", "#FF0000", NA, NA, "#FFFFFF", NA,
+                              "#FF0000", "#FF0000", "#FFFFFF", "#FF0000",
+                              "#FF0000", "#FF0000"))
+
+
+  # OR can extract the underlying data in the "correct order"
+  # according to the internal gt structure, ie arranged by group
+  # by cylinder, 6,4,8
+  gt_index(test_tab, mpg, as_vector = TRUE)
+  gt_index(test_tab, mpg, as_vector = FALSE)
+
+  sliced_arranged <- sliced_data %>%
+    dplyr::mutate(cyl = factor(cyl, levels = unique_order)) %>%
+    dplyr::arrange(cyl) %>%
+    dplyr::mutate(cyl = as.character(cyl))
+
+# Expect the values to match ----------------------------------------------
+
+
+  expect_equal(gt_index(test_tab, mpg, as_vector = FALSE),
+               sliced_arranged)
+  expect_equal(gt_index(test_tab, mpg, as_vector = TRUE),
+               c(21.4, 21, 21, 22.8, 24.4, 22.8, 14.3, 18.7, 16.4))
+})
+

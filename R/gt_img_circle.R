5d850867386c10f0355120ec135f2f62a7a82fa3
Thomas Mock <j.thomasmock@gmail.com>
2021-10-17 19:11:19 -0500

    add gt_img_circle and update docs

---
+++ R/gt_img_circle.R
@@ -0,0 +1,70 @@
+#' Create a circular border around a
+#'
+#' @param value The source image
+#' @param height The height in pixels of the circle
+#' @param border_color A string indicating the color of the border
+#' @param border_weight The weight of the border in pixels
+#' @importFrom htmltools div
+#' @importFrom glue glue
+#'
+#' @return HTML
+
+img_circle <- function(value, height, border_color, border_weight) {
+  image <- htmltools::div(
+    style = glue::glue(
+      "background-image: url({value});background-size:cover;",
+      "background-position:center;",
+      "border: {border_weight}px solid {border_color};",
+      "border-radius: 50%;height:{height}px;width:100%;"
+    )
+  )
+
+  image
+}
+
+#' Create circular border around an image
+#'
+#' @param gt_object An existing gt object
+#' @param column The column to apply the transformation to
+#' @param height A number indicating the height of the image in pixels.
+#' @param border_color The color of the circular border, can either be a single value ie (`white` or `#FF0000`) or a vector where the lenght of the vector is equal to the number of rows.
+#' @param border_weight A number indicating the weight of the border in pixels.
+#' @import gt
+#' @importFrom gt %>%
+#' @return a gt object
+#' @export
+#'
+#' @examples
+#' library(gt)
+#' gt_img_tab <- dplyr::tibble(
+#'   x = 1:4,
+#'   names = c("Rich Iannone",  "Katie Masiello", "Tom Mock","Hadley Wickham"),
+#'   img = c(
+#'     "https://pbs.twimg.com/profile_images/961326215792533504/Ih6EsvtF_400x400.jpg",
+#'     "https://pbs.twimg.com/profile_images/1123066272718049281/23XnoFUV_400x400.png",
+#'     "https://pbs.twimg.com/profile_images/1344725315684282371/R9k8sgna_400x400.jpg",
+#'     "https://pbs.twimg.com/profile_images/905186381995147264/7zKAG5sY_400x400.jpg"
+#'   )
+#' ) %>%
+#'   gt() %>%
+#'   gt_img_circle(img)
+#' @section Figures:
+#' \if{html}{\figure{gt_img_circle.png}{options: width=80\%}}
+#'
+#' @family Utilities
+#' @section Function ID:
+#' 2-15
+gt_img_circle <- function(gt_object, column, height = 25,
+                            border_color = "black", border_weight = 1.5){
+
+  stopifnot("'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?" = "gt_tbl" %in% class(gt_object))
+
+  gt_object %>%
+    text_transform(
+      locations = cells_body({{column}}),
+      fn = function(value){
+        mapply(img_circle, value, height, border_color,
+               border_weight, SIMPLIFY = FALSE)
+      }
+    )
+}

f0aa6903cd13a7b6f3f49f19032bfe7304fecf7d
Thomas Mock <j.thomasmock@gmail.com>
2021-10-21 17:31:14 -0500

    add fmt_pct_extra.R and corresponding test, rebuild docs.

---
+++ R/fmt_pct_extra.R
@@ -0,0 +1,41 @@
+#' Convert to percent and show less than 1% as <1% in grey
+#'
+#' @param gt_object An existing gt table
+#' @param columns The columns to affect
+#' @param ... Additional argument passed to `scales::label_percent()`
+#' @param scale A number to multiply values by, defaults to 1
+#'
+#' @return a gt table
+#' @export
+#'
+#' @examples
+#' pct_tab <- dplyr::tibble(x = c(.001,.05,.008,.1,.2,.5,.9)) %>%
+#'   gt::gt() %>%
+#'   fmt_pct_extra(x, scale = 100, accuracy=.1)
+#' @family Utilities
+
+fmt_pct_extra <- function(gt_object, columns, ..., scale = 1){
+
+  gt_object %>%
+    text_transform(
+      locations = cells_body(columns = {{ columns }}),
+      fn = function(x){
+        x <- as.double(x)
+
+        x <- scales::label_percent(..., scale = scale)(x)
+
+        sapply(x, function(xy){
+          xz <- gsub(x = xy, "%", "") %>% as.double()
+          if(xz >= 1){
+            xy
+          } else {
+            gt::html("<span style='color:grey;'><1%</span>")
+          }
+        })
+      }
+    ) %>%
+    cols_align("right", columns = {{ columns }})
+}
+
+
+
---
+++ tests/testthat/test-fmt_pct_extra.R
@@ -0,0 +1,31 @@
+# Function to skip tests if Suggested packages not available on system
+check_suggests <- function() {
+  skip_if_not_installed("rvest")
+  skip_if_not_installed("xml2")
+
+}
+
+test_that("fmt_pct_extra generates expected output and colors", {
+  pct_tab <- dplyr::tibble(x = c(.001,.05,.008,.1,.2,.5,.9)) %>%
+    gt::gt() %>%
+    fmt_pct_extra(x, scale = 100, accuracy=.1) %>%
+    gt::as_raw_html() %>%
+    rvest::read_html()
+
+  text_out <- pct_tab %>%
+    rvest::html_elements("td:nth-child(1)") %>%
+    rvest::html_text()
+
+ expect_equal(text_out, c("<1%", "5.0%", "<1%", "10.0%",
+                          "20.0%", "50.0%", "90.0%"))
+
+ txt_color <- pct_tab %>%
+   rvest::html_elements("td:nth-child(1) > span") %>%
+   rvest::html_attrs() %>%
+   unlist() %>%
+   unname()
+
+ expect_equal(txt_color, c("color:grey;", "color:grey;"))
+
+
+})

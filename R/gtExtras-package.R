0f660bdcfeab4b1c8545135037b2abd458523d36
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-04-11 22:09:41 -0400

    small updates

--- R/gtExtras-package.R
+++ R/gtExtras-package.R
@@ -1,9 +1,6 @@
 #' @keywords internal
 #' @import dplyr glue ggplot2 gt htmltools paletteer rlang scales
-#' @importFrom dplyr %>% between case_when slice
 #' @importFrom fontawesome fa
-#' @importFrom paletteer paletteer_d
-#' @importFrom glue glue
 "_PACKAGE"
 
 utils::globalVariables(".")

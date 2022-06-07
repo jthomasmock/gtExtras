bc3c1ce935cc9178d747094d56b728bf64e2db46
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-06-06 21:32:21 -0700

    Get everything inside this file-scoped helper

--- tests/testthat/test-fmt_symbol_first.R
+++ tests/testthat/test-fmt_symbol_first.R
@@ -1,26 +1,25 @@
-check_suggests()
-skip_on_cran()
+test_gt_by_col <- function(col_n, row_first = TRUE, expectation) {
+  check_suggests()
+  skip_on_cran()
 
-ex_gt <- gt::gtcars %>%
-  head() %>%
-  dplyr::select(mfr, year, bdy_style, mpg_h, hp) %>%
-  dplyr::mutate(mpg_h = c(20.2, 22.0, 20.8, 21.2, 22.8, 22.7)) %>%
-  gt::gt() %>%
-  gt::opt_table_font(font = google_font("Roboto Mono")) %>%
-  gt::opt_table_lines() %>%
-  fmt_symbol_first(column = mfr, symbol = "&#x24;", suffix = " ", last_row_n = 6) %>%
-  fmt_symbol_first(column = year, symbol = NULL, suffix = "%", last_row_n = 6) %>%
-  fmt_symbol_first(column = mpg_h, symbol = "&#37;", suffix = NULL, last_row_n = 6, decimals = 1) %>%
-  fmt_symbol_first(column = hp, symbol = "&#176;", suffix = "F", last_row_n = 6, decimals = NULL, symbol_first = TRUE)
+  ex_gt <- gt::gtcars %>%
+    head() %>%
+    dplyr::select(mfr, year, bdy_style, mpg_h, hp) %>%
+    dplyr::mutate(mpg_h = c(20.2, 22.0, 20.8, 21.2, 22.8, 22.7)) %>%
+    gt::gt() %>%
+    gt::opt_table_font(font = google_font("Roboto Mono")) %>%
+    gt::opt_table_lines() %>%
+    fmt_symbol_first(column = mfr, symbol = "&#x24;", suffix = " ", last_row_n = 6) %>%
+    fmt_symbol_first(column = year, symbol = NULL, suffix = "%", last_row_n = 6) %>%
+    fmt_symbol_first(column = mpg_h, symbol = "&#37;", suffix = NULL, last_row_n = 6, decimals = 1) %>%
+    fmt_symbol_first(column = hp, symbol = "&#176;", suffix = "F", last_row_n = 6, decimals = NULL, symbol_first = TRUE)
 
-ex_gt_raw <- ex_gt %>%
-  gt::as_raw_html()
+  ex_gt_raw <- ex_gt %>%
+    gt::as_raw_html()
 
-# read into rvest, and grab the table body
-ex_html_tab <- rvest::read_html(ex_gt_raw) %>%
-  rvest::html_node("table > tbody")
-
-test_gt_by_col <- function(col_n, row_first = TRUE, expectation){
+  # read into rvest, and grab the table body
+  ex_html_tab <- rvest::read_html(ex_gt_raw) %>%
+    rvest::html_node("table > tbody")
 
   # if row_first = TRUE, then just get the 1st row
   # otherwise select the remainder
@@ -42,33 +41,21 @@ test_gt_by_col <- function(col_n, row_first = TRUE, expectation){
 }
 
 test_that("fmt_symbol_first works with escaped characters", {
-  check_suggests()
-    test_gt_by_col(1, expectation = "Ford \\$")
-    test_gt_by_col(1, row_first = FALSE, expectation = "Ferrari&nbsp&nbsp")
+  test_gt_by_col(1, expectation = "Ford \\$")
+  test_gt_by_col(1, row_first = FALSE, expectation = "Ferrari&nbsp&nbsp")
 })
 
-testthat::test_that(
-  "fmt_symbol_first, Raw percent character works",
-  {
-    check_suggests()
-    test_gt_by_col(2, expectation = "2017%")
-    test_gt_by_col(2, row_first = FALSE, expectation = "201[4-7]&nbsp")
-  }
-)
+testthat::test_that("fmt_symbol_first, Raw percent character works", {
+  test_gt_by_col(2, expectation = "2017%")
+  test_gt_by_col(2, row_first = FALSE, expectation = "201[4-7]&nbsp")
+})
 
-testthat::test_that(
-  "fmt_symbol_first, HTML symbol for percent works",
-  {
-    check_suggests()
-    test_gt_by_col(4, expectation = "20.2%")
-    test_gt_by_col(4, row_first = FALSE, expectation = "[0-9]+&nbsp")
-  }
-)
+testthat::test_that("fmt_symbol_first, HTML symbol for percent works", {
+  test_gt_by_col(4, expectation = "20.2%")
+  test_gt_by_col(4, row_first = FALSE, expectation = "[0-9]+&nbsp")
+})
 
-testthat::test_that(
-  "fmt_symbol_first, A combined suffix + symbol work",
-  {
-    test_gt_by_col(5, expectation = "647°F")
-    test_gt_by_col(5, row_first = FALSE, expectation = "[0-9]+&nbsp&nbsp")
-  }
-)
+testthat::test_that("fmt_symbol_first, A combined suffix + symbol work", {
+  test_gt_by_col(5, expectation = "647°F")
+  test_gt_by_col(5, row_first = FALSE, expectation = "[0-9]+&nbsp&nbsp")
+})

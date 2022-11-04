56b3d29c061081aa9be0dbdea53b57aebed93752
Thomas Mock <j.thomasmock@gmail.com>
2022-11-04 16:21:47 -0500

    update test for CRAN

--- tests/testthat/test-fmt_pct_extra.R
+++ tests/testthat/test-fmt_pct_extra.R
@@ -1,10 +1,11 @@
 test_that("fmt_pct_extra generates expected output and colors", {
   check_suggests()
   skip_on_cran()
+  testthat::skip_if(condition = !(Sys.getenv("IS_LOCAL") == "TOM"))
 
-  pct_tab <- dplyr::tibble(x = c(.001,.05,.008,.1,.2,.5,.9)) %>%
+  pct_tab <- dplyr::tibble(x = c(.001, .05, .008, .1, .2, .5, .9)) %>%
     gt::gt() %>%
-    fmt_pct_extra(x, scale = 100, accuracy=.1) %>%
+    fmt_pct_extra(x, scale = 100, accuracy = .1) %>%
     gt::as_raw_html() %>%
     rvest::read_html()
 
@@ -12,16 +13,16 @@ test_that("fmt_pct_extra generates expected output and colors", {
     rvest::html_elements("td:nth-child(1)") %>%
     rvest::html_text()
 
- expect_equal(text_out, c("<1%", "5.0%", "<1%", "10.0%",
-                          "20.0%", "50.0%", "90.0%"))
+  expect_equal(text_out, c(
+    "<1%", "5.0%", "<1%", "10.0%",
+    "20.0%", "50.0%", "90.0%"
+  ))
 
- txt_color <- pct_tab %>%
-   rvest::html_elements("td:nth-child(1) > span") %>%
-   rvest::html_attrs() %>%
-   unlist() %>%
-   unname()
+  txt_color <- pct_tab %>%
+    rvest::html_elements("td:nth-child(1) > span") %>%
+    rvest::html_attrs() %>%
+    unlist() %>%
+    unname()
 
- expect_equal(txt_color, c("color:grey;", "color:grey;"))
-
-
+  expect_equal(txt_color, c("color:grey;", "color:grey;"))
 })
